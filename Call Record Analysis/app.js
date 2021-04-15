83638bfb7b44ba0392277813faaf9c98ba48dfd9
Your Name <you@example.com>
2021-04-15 15:41:23 -0400

    Added new files to Call Record Analysis

---
+++ Call Record Analysis/app.js
@@ -0,0 +1,52 @@
+let obj = require("mongoose");  //load the module 
+obj.Promise = global.Promise;       // creating the reference. 
+let url = "mongodb://localhost:27017/meanstack";
+let fs = require('fs');
+const mongooseDbOption = {       // to avoid warning 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+obj.connect(url, mongooseDbOption);   //ready to connect 
+let db = obj.connection;    // connected to database. 
+db.on("error", (err) => console.log(err));
+db.once("open", () => {
+
+    //Defined the Schema 
+    let CallRecordSchema = obj.Schema({
+        _id: Number,
+        source: String,
+        destination: String,
+        sourceLocation: String,
+        destinationLocation: String,
+        callDuration: String,
+        roaming: String,
+        callCharge: String,
+    });
+    // Creating Model using schema 
+    let callRecord = obj.model("", CallRecordSchema, "Call_data");
+    fs.readFile("call_data.json", (err, data) => {
+        if (!err) {
+            //console.log(data.toString());
+            let callRecordString = data.toString();
+            let callRecordJson = JSON.parse(callRecordString);
+
+
+
+            // Creating reference using model 
+            callRecordJson.forEach(element => {
+                let call1 = new callRecord(element)
+
+                call1.save((err, result) => {
+                    if (!err) {
+                        console.log("record inserted successfully" + result)
+                    } else {
+                        console.log(err);
+                    }
+                    obj.disconnect();       //close the connectiond..
+                })
+            })
+        }
+    })
+
+})
+
