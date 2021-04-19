cfad89195e4efbd288fb7b61e92fa86ccc2edb32
Your Name <you@example.com>
2021-04-19 12:15:28 -0400

    Added new files to Chat Log

--- Chat Log/server.js
+++ Chat Log/server.js
@@ -11,52 +11,56 @@ const mongooseDbOption = {       // to avoid warning
 }
 
 const CallSchema = obj.Schema({
-    name:String,
-    message:String
+    _id:Number,
+    name: String,
+    message: String
 });
 
 
-app.get("/",(req,res)=> {
-    res.sendFile(__dirname+"/index.html");
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 })
 
 var name1;
 var msg;
-
-io.on("connection",(socket)=> {
+var count = 0;
+io.on("connection", (socket) => {
     console.log("Client connected to application.....");
-    
-    socket.on("name",(name)=> {
+
+    socket.on("name", (name) => {
         console.log("Hello: " + name);
         name1 = name;
     })
 
-    socket.on("message",(message)=> {
+    socket.on("message", (message) => {
+
         console.log("Your: " + message);
         msg = message;
+        count++;
+
+
+        obj.connect(url, mongooseDbOption);   //ready to connect 
+        let db = obj.connection;    // connected to database. 
+        db.on("error", (err) => console.log(err));
+        db.once("open", () => {
+
+            //Defined the Schema 
+
+            // Creating Model using schema 
+            let callRecord = obj.model("", CallSchema, "Call_chat");
+
+            let call1 = new callRecord({ _id:count,name: name1, message: msg })
+
+            call1.save((err, result) => {
+                if (!err) {
+                    console.log("record inserted successfully" + result)
+                } else {
+                    console.log(err);
+                }
+                obj.disconnect();       //close the connectiond..
+            })
+        })
     })
-
-    obj.connect(url, mongooseDbOption);   //ready to connect 
-let db = obj.connection;    // connected to database. 
-db.on("error", (err) => console.log(err));
-    db.once("open", () => {
-
-        //Defined the Schema 
-        
-        // Creating Model using schema 
-        let callRecord = obj.model("", CallSchema, "Call_chat");
-    
-                    let call1 = new callRecord({name:name1,message:msg})
-    
-                    call1.save((err, result) => {
-                        if (!err) {
-                            console.log("record inserted successfully" + result)
-                        } else {
-                            console.log(err);
-                        }
-                        obj.disconnect();       //close the connectiond..
-                    })
-                })
 })
 
 
@@ -64,4 +68,4 @@ db.on("error", (err) => console.log(err));
 
 
 
-http.listen(9090,()=>console.log('server running on port number 9090'));
+http.listen(9090, () => console.log('server running on port number 9090'));
