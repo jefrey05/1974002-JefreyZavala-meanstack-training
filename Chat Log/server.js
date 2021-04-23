9ee63e5bc8e52e6864f66e9e705d34ff9f9e22d3
Your Name <you@example.com>
2021-04-23 13:59:01 -0400

    Chat log

--- Chat Log/server.js
+++ Chat Log/server.js
@@ -1,14 +1,71 @@
-var app = express();
-var admin = express();
-app.get('/',function(req,res){
-    console.log(req.protocol);
-    res.send('page');
-})
-admin.get('/user',function(req,res){
-    res.send('Admind Homepage | User');
+let app = require("express")();
+let http = require("http").Server(app);   // to load the library we have run port number using hhtp module 
+let io = require("socket.io")(http);
+let obj = require("mongoose");  //load the module 
+obj.Promise = global.Promise;       // creating the reference. 
+let url = "mongodb://localhost:27017/meanstack";
+let fs = require('fs');
+const mongooseDbOption = {       // to avoid warning 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const CallSchema = obj.Schema({
+    _id:Number,
+    name: String,
+    message: String
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 })
 
-app.use('/admin',admin);
-app.get('*',function(req,res){
-    res.send('404');
+var name1;
+var msg;
+var count = 0;
+io.on("connection", (socket) => {
+    console.log("Client connected to application.....");
+
+    socket.on("name", (name) => {
+        console.log("Hello: " + name);
+        name1 = name;
+    })
+
+    socket.on("message", (message) => {
+
+        console.log("Your: " + message);
+        msg = message;
+        count++;
+
+
+        obj.connect(url, mongooseDbOption);   //ready to connect 
+        let db = obj.connection;    // connected to database. 
+        db.on("error", (err) => console.log(err));
+        db.once("open", () => {
+
+            //Defined the Schema 
+
+            // Creating Model using schema 
+            let callRecord = obj.model("", CallSchema, "Call_chat");
+
+            let call1 = new callRecord({ _id:count,name: name1, message: msg })
+
+            call1.save((err, result) => {
+                if (!err) {
+                    console.log("record inserted successfully" + result)
+                } else {
+                    console.log(err);
+                }
+                obj.disconnect();       //close the connectiond..
+            })
+        })
+    })
 })
+
+
+
+
+
+
+http.listen(9090, () => console.log('server running on port number 9090'));
